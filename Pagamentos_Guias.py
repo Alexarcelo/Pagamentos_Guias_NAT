1daa436901e2da4a285c0282dab756af47646b9b
Alexarcelo <marcelo@luckreceptivo.com.br>
2025-01-03 17:27:20 -0300

    Update Pagamentos_Guias.py

--- Pagamentos_Guias.py
+++ Pagamentos_Guias.py
@@ -10,6 +10,8 @@ from google.cloud import secretmanager
 import json
 from google.oauth2.service_account import Credentials
 
+# Atual
+
 def gerar_df_phoenix(vw_name, base_luck):
 
     # Parametros de Login AWS
